718160124afcdd3c1a39a998e5c7c3dd276ffdbf
Albert <albert.dugba@gmail.com>
2022-04-09 02:31:29 +0000

    setup eslint

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+// lint-staged.config.js
+module.exports = {
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+}
