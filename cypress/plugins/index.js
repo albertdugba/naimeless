68eec244f6c8c708d9a3221fc89ec34de9bd1ecf
Albert <albert@montech.io>
2022-08-31 13:40:02 +0000

    fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,28 +2,11 @@
 /// <reference types="cypress" />
 
 import cypressReplay from '@replayio/cypress'
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 export default (on, config) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('cypress-log-to-output').install(on)
 
-  cypressReplay.default(on, config)
+  cypressReplay(on, config)
 
   return config
 }
